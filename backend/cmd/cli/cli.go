4da80043ae05cf191522d56c834c18b51c99f062
Ronit Mishra <ronnie94official@gmail.com>
2021-08-23 05:41:03 +0530

    Added ns command

---
+++ backend/cmd/cli/cli.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+
+	app := cli.NewApp()
+	app.Name = "Website Lookup CLI"
+	app.Usage = "Let's you query IPs, CNAME, MX records and Name Servers"
+
+	// We'll be using the same flag for all our commands
+    // so we'll define it up here
+    myFlags := []cli.Flag{
+        cli.StringFlag{
+            Name:  "host",
+            Value: "ronnie.tech",
+        },
+    }
+
+    // we create our commands
+    app.Commands = []cli.Command{
+        {
+            Name:  "ns",
+            Usage: "Looks Up the NameServers for a Particular Host",
+            Flags: myFlags,
+            // the action, or code that will be executed when
+            // we execute our `ns` command
+            Action: func(c *cli.Context) error {
+                // a simple lookup function
+                ns, err := net.LookupNS(c.String("host"))
+                if err != nil {
+                    return err
+                }
+                // we log the results to our console
+                // using a trusty fmt.Println statement
+                for i := 0; i < len(ns); i++ {
+                    fmt.Println(ns[i].Host)
+                }
+                return nil
+            },
+        },
+    }
+
+	// init application
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
